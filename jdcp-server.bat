105e3978194ce7389ea371d70f1598c2f757e41a
Brad Kimmel <intrepidca@gmail.com>
2008-12-21 23:17:14 +0000

    Added manifests to jars built by ant jar tasks. Renamed jdcpserver.bat to jdcp-server.bat. Added shell script for starting the server. Modified ant "dist-server" target to put the batch file and shell script into the distribution.

---
+++ jdcp-server.bat
@@ -0,0 +1,1 @@
+java -Djava.library.path=lib -Djava.security.manager -Djava.security.auth.login.config=etc/login.config -Djava.security.policy=etc/policy -Dlog4j.configuration=file:./etc/log4j.properties -jar jdcp-server.jar
--- jdcpserver.bat
+++
@@ -1,1 +0,0 @@
-java -Djava.library.path=lib -Djava.security.manager -Djava.security.auth.login.config=login.config -Djava.security.policy=policy -Dlog4j.configuration=file:./log4j.properties -jar jdcpserver.jar
