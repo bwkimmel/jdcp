098790e445e44cd4d0dd64eb389bd2f1701f8bd8
Brad Kimmel <intrepidca@gmail.com>
2009-08-05 21:17:51 +0000

    Added some parameters for shell scripts and fixed the line endings.

--- jdcp-console.sh
+++ jdcp-console.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Djava.library.path=lib -Djava.security.manager -Djava.security.auth.login.config=etc/login.config -Djava.security.policy=etc/policy -Dlog4j.configuration=file:./etc/log4j.properties -jar jdcp-console.jar
+java -Djava.rmi.server.randomIDs=true -Djava.rmi.server.disableHttp=true -Djava.library.path=lib -Djava.security.manager -Djava.security.auth.login.config=etc/login.config -Djava.security.policy=etc/policy -Dlog4j.configuration=file:./etc/log4j.properties -jar jdcp-console.jar
