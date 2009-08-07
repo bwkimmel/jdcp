8490e6582e211e88271cb96f54f46f03f4e58f4e
Brad Kimmel <intrepidca@gmail.com>
2009-08-07 00:19:27 +0000

    Modified console .bat file so that the commands are not echoed.

--- jdcp-console.bat
+++ jdcp-console.bat
@@ -1,1 +1,2 @@
+@echo off
 java -Djava.library.path=lib -Djava.security.manager -Djava.security.auth.login.config=etc/login.config -Djava.security.policy=etc/policy -Dlog4j.configuration=file:./etc/log4j.properties -jar jdcp-console.jar
