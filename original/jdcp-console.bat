fc1ee5d210112cf82bac589042541e8e36823599
Brad Kimmel <intrepidca@gmail.com>
2014-08-14 00:24:00 -0400

    Remove original

--- original/jdcp-console.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java -Djava.library.path=lib -Djava.security.manager -Djava.security.auth.login.config=etc/login.config -Djava.security.policy=etc/policy -Dlog4j.configuration=file:./etc/log4j.properties -jar jdcp-console.jar
--- original/jdcp-console.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -server -Djava.net.preferIPv4Stack=true -Djava.rmi.server.randomIDs=true -Djava.rmi.server.disableHttp=true -Djava.library.path=lib -Djava.security.manager -Djava.security.auth.login.config=etc/login.config -Djava.security.policy=etc/policy -Dlog4j.configuration=file:./etc/log4j.properties -jar jdcp-console.jar $*
